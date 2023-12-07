07aaa92f003aa62feb0486863ab7efe09357a948
MajorTripp <110078432+MajorTripp@users.noreply.github.com>
2023-12-06 20:54:09 -0800

    Add files via upload

---
+++ FinalProject/src/main/java/com/mycompany/finalproject/UsersFacade.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.finalproject;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+/**
+ *
+ * @author jerad
+ */
+@Stateless
+public class UsersFacade extends AbstractFacade<Users> {
+
+    @PersistenceContext(unitName = "my_persistence_unit")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UsersFacade() {
+        super(Users.class);
+    }
+    
+}
