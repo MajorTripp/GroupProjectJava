a94aead57d5820cdeefd0a3b63e6102ec56078e8
bloblehead <104156526+bloblehead@users.noreply.github.com>
2023-12-09 08:49:33 -0600

    password

---
+++ FinalProject/src/main/webapp/resources/js/pass.js
@@ -0,0 +1,21 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/Gruntfile.js to edit this template
+ */
+
+//TODO learn javascript
+
+function checkPassword(){
+    alert("this should do a thing");
+}
+
+
+
+
+/*
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
+*/
